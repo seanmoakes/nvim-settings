13ac409982f09030572f48c282b35a94e5eaab8d
Sean Moakes <seanmoakes@hotmail.com>
2023-01-23 13:26:51 +0000

    first content commit

---
+++ after/plugin/telescope.lua
@@ -0,0 +1,7 @@
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+vim.keymap.set('n', '<leader>sg', builtin.git_files, { desc = '[S]earch [G]it Files' })
+vim.keymap.set('n', '<leader>sw', function()
+	builtin.grep_string({ search = vim.fn.input("Grep > ") })
+end)
+vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp'})
